280a48c5f87e19d008f15065e9fc070c0bfb55ff
Ari Shtein <ashtein120@gmail.com>
2022-09-29 19:59:57 -0400

    Add tree-creating method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,22 +1,46 @@
+require_relative 'node'
 require 'pry-byebug'
 
 class Tree
-  attr_reader :root
-
   def initialize(arr)
-    @root = build_tree(arr)
+    sorted_array = sort(arr)
+    nice_array = clean(sorted_array)
+    @root = build_tree(nice_array)
   end
 
   def build_tree(array)
-    sort(array).uniq!
+    # binding.pry
+    return nil if array.empty?
+
+    first = 0
+    last = array.size - 1
+    mid = (first + last) / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array[first...mid])
+    root.right = build_tree(array[(mid + 1)..last])
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def clean(array)
+    clean_arr = []
+    array.each do |val|
+      clean_arr << val unless clean_arr.include?(val)
+    end
+    clean_arr
   end
 
   def sort(array)
     length = array.size
     return array if length < 2
 
-    left_half = sort(array[0...length / 2])
-    right_half = sort(array[length / 2...length])
+    left_half = sort(array[0...(length / 2)])
+    right_half = sort(array[(length / 2)...length])
     merge(left_half, right_half)
   end
 
@@ -39,4 +63,10 @@ class Tree
   end
 end
 
-Tree.new([1, 5, 3, 6, 5, 2])
+cool_array = []
+
+(1..128).each do |i|
+  cool_array << i
+end
+
+Tree.new(cool_array).pretty_print
